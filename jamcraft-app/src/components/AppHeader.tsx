053356cc77588b56cdc2e7b27558659c3d94a340
James Herr <79765141+SeveralHerr@users.noreply.github.com>
2025-02-07 16:54:42 -0600

    Added socials

--- jamcraft-app/src/components/AppHeader.tsx
+++ jamcraft-app/src/components/AppHeader.tsx
@@ -1,8 +1,11 @@
-import { Group } from '@mantine/core';
+import { Group, Space, ActionIcon } from '@mantine/core';
+import { IconBrandBluesky , IconBrandLinkedinFilled, IconBrandYoutubeFilled,IconBrandSteamFilled,IconBrandGithubFilled    } from '@tabler/icons-react';
+
 import classes from './css/AppHeader.module.css';
 
 const links = [
-  { link: '/about', label: 'About' },
+  { link: '/about', label: 'about' },
+  { link: '/games', label: 'games' },
 ];
 
 export function AppHeader() {
@@ -20,16 +23,48 @@ export function AppHeader() {
   ));
 
   return (
+    
     <header className={classes.header}>
       <div className={classes.inner}>
-        <Group>
 
-        </Group>
+  
+          <Group ml='lg' gap={54}visibleFrom="sm" justify='flex-end' >
 
-        <Group>
-          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+          </Group>
+          
+
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+        <Group ml='lg' gap={30} className={classes.links} visibleFrom="sm" justify='flex-end' >
             {items}
           </Group>
+          <Space w="lg" />
+          <Space w="lg" />
+          <Space w="lg" />
+          <ActionIcon size="lg" variant="light" radius="xl"color="white">
+            <IconBrandLinkedinFilled size={36} stroke={1.5} />
+          </ActionIcon>
+          <Space w="lg" />
+          <ActionIcon size="lg" variant="light" radius="xl" color="white">
+            <IconBrandYoutubeFilled  size={36} stroke={1.5} />
+          </ActionIcon>
+          <Space w="lg" />
+          <ActionIcon size="lg" variant="light" radius="xl"color="white">
+            <IconBrandBluesky  size={36} stroke={1.5} />
+          </ActionIcon>
+          <Space w="lg" />
+          <ActionIcon size="lg" variant="light" radius="xl"color="white">
+            <IconBrandSteamFilled   size={36} stroke={1.5} />
+          </ActionIcon>
+          <Space w="lg" />
+          <ActionIcon size="lg" variant="light" radius="xl"color="white">
+            <IconBrandGithubFilled    size={36} stroke={1.5} />
+          </ActionIcon>
+          <Space w="lg" />
+          <Space w="lg" />
+          <Space w="lg" />
+          <Space w="lg" />
+          <Space w="lg" />
+          <Space w="lg" />
         </Group>
       </div>
     </header>
